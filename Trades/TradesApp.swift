5e76ed469987f6b9cd9621e92919984542760525
Mustafa Emir Ata <mustafaemirata@outlook.com>
2026-01-29 12:26:08 +0300

    Initial Commit

---
+++ Trades/TradesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradesApp.swift
+//  Trades
+//
+//  Created by Mustafa Emir Ata on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TradesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TradesTests/TradesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TradesTests.swift
+//  TradesTests
+//
+//  Created by Mustafa Emir Ata on 29.01.2026.
+//
+
+import Testing
+@testable import Trades
+
+struct TradesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
